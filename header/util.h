aae50e90cb343b7014929e8995efaebb9e2422df
francis-de-leon <francis_zaccharie_deleon@dlsu.edu.ph>
2022-05-17 22:02:28 +0800

    created util.h and defined struct

---
+++ header/util.h
@@ -0,0 +1,25 @@
+#ifndef UTIL
+#define UTIL
+
+// include libraries
+#include <stdio.h>
+
+
+// type definitions
+typedef char string5[6];
+typedef char string11[12];
+typedef char string300[301];
+
+
+// struct definition
+typedef struct 
+{
+    string11 sFull_Name;
+    string5 sShort_Name;
+    string300 sDesc;
+    char cGender;
+    short nCaught;
+} mon_type;
+
+
+#endif
--- src/main.c
+++ src/main.c
@@ -2,11 +2,6 @@
 
 int main(void)
 {
-    printf("\x1b[4m");
-    printf("> Hello World!\n");
-
-    printf("\x1b[0m");
-    printf("%c---------------------------------%c\n", 218, 1022);
-
+    
     return 0;
 }
