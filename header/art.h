f48ae3a7a781d12f7dc32e53faf3fec8ec31e653
francis-de-leon <francis_zaccharie_deleon@dlsu.edu.ph>
2022-05-18 11:10:43 +0800

    added art

---
+++ header/art.h
@@ -0,0 +1,13 @@
+#ifndef ART_H
+#define ART_H
+
+#include "../configurations.h"
+
+char HDR_FakeDex[][WIDTH] = 
+{
+    {" ____   __    _     ____  ___   ____  _    "},
+    {"| |_   / /\\  | |_/ | |_  | | \\ | |_  \\ \\_/ "},
+    {"|_|   /_/--\\ |_| \\ |_|__ |_|_/ |_|__ /_/ \\ "},
+};
+
+#endif
