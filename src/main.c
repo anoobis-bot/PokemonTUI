f267ce3906d8e58b847c077dafe593f6f62529fa
francis-de-leon <francis_zaccharie_deleon@dlsu.edu.ph>
2022-05-18 09:54:59 +0800

    added intro comment and dex array

--- src/main.c
+++ src/main.c
@@ -1,9 +1,25 @@
-#include "../header/util.h"
+/******************************************
+File Name: main.c
 
-#include <stdio.h>
+Author: Francis Zaccharie B. de Leon
+Last Update: May 18, 2022
+
+This is the highest level of source code.
+It contains just function calls and simple
+return values.
+******************************************/
+
+// included header files
+#include "../header/util.h"
+#include "../configurations.h"
+
 
 int main(void)
 {
-    
+    // initialize dex with 0s
+    mon_type FakeDex[DEX_MAX] = {0};
+
+
+
     return 0;
 }
