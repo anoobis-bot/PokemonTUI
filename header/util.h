aab69792c76dab1b336860b9bae06aca63402586
francis-de-leon <francis_zaccharie_deleon@dlsu.edu.ph>
2022-05-18 09:55:21 +0800

    added CLEAR macro

--- header/util.h
+++ header/util.h
@@ -3,12 +3,17 @@
 
 // include libraries
 #include <stdio.h>
+#include <string.h>
 
 
 // type definitions
 typedef char string5[6];
 typedef char string11[12];
 typedef char string300[301];
+
+
+// macro define
+#define CLEAR "\e[1;1H\e[2J"
 
 
 // struct definition
