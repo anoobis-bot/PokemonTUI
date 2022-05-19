7512233f13ac985b9b68c6863426110e390aa9f0
francis-de-leon <francis_zaccharie_deleon@dlsu.edu.ph>
2022-05-19 17:45:15 +0800

    changed hieght and subsituted the box corners

--- configurations.h
+++ configurations.h
@@ -1,22 +1,29 @@
 #ifndef CONFIGURATION
 #define CONFIGURATION
 
-#define HEIGHT 16
-#define WIDTH 84        // content width
+#define HEIGHT 17
+#define WIDTH 75        // content width
 
-#define LEFT_MARGIN 2  // margin on the left side of TUI
+#define LEFT_MARGIN 2   // margin on the left side of TUI
 
 #define RIGHT_PAD 1     // padding on the right side of TUI
 #define LEFT_PAD 1      // padding on the left side of TUI
 
 #define ART_TEXT_HEIGHT 3
 
-#define UP_LEFT_CORNER 191      // "┌"
-#define UP_RIGHT_CORNER 218     // "┐"
+#define UP_LEFT_CORNER 218      // "┌"
+#define UP_RIGHT_CORNER 191     // "┐"
+#define MID_LEFT_CORNER 195     // ├
+#define MID_RIGHT_CORNER 180    // ┤
 #define DOWN_LEFT_CORNER 192    // "└"
 #define DOWN_RIGHT_CORNER 217   // "┘"
-#define H_LINE "|"
+#define V_LINE "|"
+#define H_LINE "-"
 
 #define DEX_MAX 150
+
+#define STR_MARGIN 2
+
+#define STR_IO_SIZE 65
 
 #endif
