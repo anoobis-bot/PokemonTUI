0ba55be6b4cfa9f481949414f7458423c76a9d82
francis-de-leon <francis_zaccharie_deleon@dlsu.edu.ph>
2022-06-12 11:03:57 +0800

    main.c :: added function comments and added variable name prefixes

--- src/main.c
+++ src/main.c
@@ -16,11 +16,16 @@ return values.
 #include "../protoypes/box_imp_proto.h"
 
 
+/* The main function serves as the main flow director of the program. It has the highest level of implementation
+    that is why it is mostly only function calls here and string choices declaration.
+    It controls which functions to go to based on the user input.
+    Each main section (i.e., Fakedex, Exploration, etc..) has its own function.
+*/
 int main(void)
 {
     // choices in the game
     // Main Menu Choices
-    stringChoice sMainChoices[MAINCHOICES_SIZE] = {"Fakedex", "Exploration", "Box", "Settings", "Exit"};  // main menu choices
+    stringChoice sMainChoices[MAINCHOICES_SIZE] = {"Fakedex", "Exploration", "Box", "Settings", "Exit"};
     // Fakedex Database Choices
     stringChoice sDatabaseChoices[DATABASECHOICES_SIZE] = {"Add Dex", "View Dex", "Update Dex", "Remove Dex", "Cancel"};
     // View fakemon entry choices
@@ -37,7 +42,7 @@ int main(void)
 
     
     // initialize dex and make all members equal to 0
-    mon_type FakeDex[DEX_MAX] = {{{0}}};
+    mon_type pFakeDex[DEX_MAX] = {{{0}}};
 
     // sizes of each member in the FakeDex struct (mon_type)
     int nDatabase_In_Sizes[STRUCT_IN_NUM] = {FULL_NAME_SIZE + STR_MARGIN, SHORT_NAME_SIZE + STR_MARGIN, DESCRIPTION_SIZE + STR_MARGIN, 
@@ -47,18 +52,18 @@ int main(void)
     int nMonCreated = 0;
 
     // array of box_type that tell which fakemon are already caught. the number of each element identifies to
-    // which fakemon it is based on the index number of the fakemon in Fakedex[].
-    // if caughtMons[0] = 1. then that means that the trainer has the fakemon of fakedex's index 1.
-    box_type caughtMons[BOX_MAX] = {{0}};
+    // which fakemon it is based on the index number of the fakemon in pFakeDex[].
+    // if pCaughtMons[0] = 1. then that means that the trainer has the fakemon of fakedex's index 1.
+    box_type pCaughtMons[BOX_MAX] = {{0}};
 
     // number of mons already in the box (caught mons)
     int nCapturedMons = 0;
 
     // selected fakemon in the Fakdex database
-    int mon_Sel = -1;
+    int nMon_Sel = -1;
 
     // variable used to keep track of the position of the player (Exploration Mode)
-    int ActiveCell = 0;
+    int nActiveCell = 0;
 
     // sInput is the variable that will hold the user inputt
     stringIn sInput;
@@ -68,7 +73,6 @@ int main(void)
 
     // used to catch the return value of functions whether the operation was succesful or not. default to no
     int isSucces = 0;
-
 
 
     // seeding the rand() function
@@ -88,8 +92,8 @@ int main(void)
                 if (strcmp(sInput, sDatabaseChoices[0]) == 0)
                 {
                     
-                    isSucces = addDex(sInput, nDatabase_In_Sizes, STRUCT_IN_NUM, FakeDex,   // Add Dex TUI
-                                        &nMonCreated, caughtMons, nCapturedMons, sMessage, -1);  
+                    isSucces = addDex(sInput, nDatabase_In_Sizes, STRUCT_IN_NUM, pFakeDex,   // Add Dex TUI
+                                        &nMonCreated, pCaughtMons, nCapturedMons, sMessage, -1);  
                     if (isSucces)   // addDex returns 1 if new fakemon populated the Fakedex (excluding update entries)
                         nMonCreated++;
                 }
@@ -97,25 +101,25 @@ int main(void)
                 else if (strcmp(sInput, sDatabaseChoices[1]) == 0)
                 {
                     do {
-                        mon_Sel = viewDex(sInput, STR_INPUT_STD + STR_MARGIN, FakeDex,      // View Dex TUI
+                        nMon_Sel = viewDex(sInput, STR_INPUT_STD + STR_MARGIN, pFakeDex,      // View Dex TUI
                                             nMonCreated, sMessage);
-                        // mon_Sel is -1 if no fakemon in the fakedex is selected (if the user typed cancel)
+                        // nMon_Sel is -1 if no fakemon in the fakedex is selected (if the user typed cancel)
                         // there are no -1 in the index of the array Fakemon[]
-                        if (mon_Sel != -1)  
+                        if (nMon_Sel != -1)  
                             viewMon(sInput, STR_INPUT_STD + STR_MARGIN, sViewMonDexChoices, VIEWMONDEXCHOICES_SIZE, 
-                                        FakeDex, mon_Sel, 0,sMessage);
-                    } while (mon_Sel != -1);
+                                        pFakeDex, nMon_Sel, 0,sMessage);
+                    } while (nMon_Sel != -1);
                 }
                 // Update Dex
                 else if (strcmp(sInput, sDatabaseChoices[2]) == 0)
                 {
-                    updateDex(sInput, nDatabase_In_Sizes, STRUCT_IN_NUM, FakeDex,   // Update Dex TUI
-                        &nMonCreated, caughtMons, nCapturedMons, sMessage);
+                    updateDex(sInput, nDatabase_In_Sizes, STRUCT_IN_NUM, pFakeDex,   // Update Dex TUI
+                        &nMonCreated, pCaughtMons, nCapturedMons, sMessage);
                 }
                 // Remove Dex
                 else if (strcmp(sInput, sDatabaseChoices[3]) == 0)
                 {
-                    removeDex(sInput, FULL_NAME_SIZE + STR_MARGIN, FakeDex, &nMonCreated, caughtMons, 
+                    removeDex(sInput, FULL_NAME_SIZE + STR_MARGIN, pFakeDex, &nMonCreated, pCaughtMons, 
                                 nCapturedMons, sMessage);
                 }
 
@@ -132,14 +136,14 @@ int main(void)
                 do 
                 {
                     exploration(sInput, STR_INPUT_STD + STR_MARGIN, sExplorationChoices, EXPLORATIONCHOICES_SIZE, 
-                                    &ActiveCell, nMonCreated, sMessage);
+                                    &nActiveCell, nMonCreated, sMessage);
 
                     // if the user has not typed "Cancel"
                     if (strcmp(sInput, sExplorationChoices[EXPLORATIONCHOICES_SIZE - 1]) != 0)
                     {
                         // encounter a fakemon. randomized fakemon is handled inside the function
                         encounter(sInput, STR_INPUT_STD + STR_MARGIN, sEncounterChoices, ENCOUNTERCHOICES_SIZE, 
-                                    FakeDex, nMonCreated, caughtMons, &nCapturedMons, sMessage);
+                                    pFakeDex, nMonCreated, pCaughtMons, &nCapturedMons, sMessage);
                     }   
                                     
 
@@ -162,7 +166,7 @@ int main(void)
                 }
 
                 // either way, if the user exited the exploration, his position will repeat to index 0
-                ActiveCell = 0;
+                nActiveCell = 0;
             }
             // if there is no more room
             else
@@ -177,8 +181,8 @@ int main(void)
         {
             do
             {
-                viewBox(sInput, STR_INPUT_STD + STR_MARGIN, sBoxChoices, BOXCHOICES_SIZE, FakeDex, 
-                            caughtMons, &nCapturedMons, sMessage);
+                viewBox(sInput, STR_INPUT_STD + STR_MARGIN, sBoxChoices, BOXCHOICES_SIZE, pFakeDex, 
+                            pCaughtMons, &nCapturedMons, sMessage);
             } while (strcmp(sInput, sBoxChoices[BOXCHOICES_SIZE - 1]) != 0);
             // while the user has not typed cancel
 
@@ -201,20 +205,25 @@ int main(void)
                 // save
                 if (strcmp(sInput, sSettingChoices[0]) == 0)
                 {
-                    save(sInput, FILE_NAME_LEN + STR_MARGIN, nMonCreated, FakeDex, caughtMons, 
+                    save(sInput, FILE_NAME_LEN + STR_MARGIN, nMonCreated, pFakeDex, pCaughtMons, 
                             nCapturedMons, sMessage); // save TUI
                 }
                 // load
                 else if (strcmp(sInput, sSettingChoices[1]) == 0)
                 {
-                    load(sInput, FILE_NAME_LEN + STR_MARGIN, &nMonCreated, FakeDex, caughtMons, 
+                    load(sInput, FILE_NAME_LEN + STR_MARGIN, &nMonCreated, pFakeDex, pCaughtMons, 
                             &nCapturedMons, sMessage);
                 }
 
             } while (strcmp(sInput, sSettingChoices[SETTINGCHOICES_SIZE - 1]) != 0); 
             // while the user has not typed "Cancel"
         }
-    } while (strcmp(sInput, sMainChoices[MAINCHOICES_SIZE - 1]) != 0);  // while the user has not typed "Exit"
+    
+    } while (strcmp(sInput, sMainChoices[MAINCHOICES_SIZE - 1]) != 0);
+    // while the user has not typed "Exit". This terminates the Program if the while loop stops
+
+    printf(CLEAR);
+    printf("Program Terminated.\n\n");
 
     return 0;
 }
